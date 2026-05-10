6ab6d9606d712e5d5d49297650ec925f479fcf03
Hector Triana <ptb1204@hotmail.com>
2026-05-10 15:14:44 -0500

    feat: proposal cypress alerts approach

--- cypress/e2e/alerts.cy.js
+++ cypress/e2e/alerts.cy.js
@@ -1,25 +1,47 @@
-describe('Cypress application', () => {
-  before(() => {
+/// <reference types="cypress" />
 
+describe('DemoQA Alerts Automation', () => {
+  beforeEach(() => {
+    cy.visit('/alerts');
   });
 
   it('should have the ability to assert automatically resolved alerts', () => {
-
+    cy.get('#alertButton').click();
+    cy.on('window:alert', (alertText) => {
+      expect(alertText).to.equal('You clicked a button');
+    });
   });
 
-  it('should have the ability to assert scheduled allert', () => {
-
+  it('should have the ability to assert scheduled alert', () => {
+    cy.get('#timerAlertButton').click();
+    cy.on('window:alert', (alertText) => {
+      expect(alertText).to.equal('This alert appeared after 5 seconds');
+    });
   });
 
-  it('should autimatically resolve alerts', () => {
-
+  it('should automatically resolve alerts', () => {
+    cy.get('#confirmButton').click();
+    cy.on('window:confirm', (confirmText) => {
+      expect(confirmText).to.equal('Do you confirm action?');
+      return true;
+    });
+    cy.get('#confirmResult').should('contain', 'You selected Ok');
   });
 
   it('should have the ability to Cancel alerts', () => {
-
+    cy.get('#confirmButton').click();
+    cy.on('window:confirm', (confirmText) => {
+      expect(confirmText).to.equal('Do you confirm action?');
+      return false;
+    });
+    cy.get('#confirmResult').should('contain', 'You selected Cancel');
   });
 
   it('should have the ability to enter text to alert', () => {
-
+    cy.window().then((win) => {
+      cy.stub(win, 'prompt').returns('AORUS');
+    });
+    cy.get('#promtButton').click();
+    cy.get('#promptResult').should('contain', 'You entered AORUS');
   });
 });
